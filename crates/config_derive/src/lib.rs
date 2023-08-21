d0d47db5306578a814570c60419794f91e48b688
miller <psnewer@163.com>
2023-08-21 20:09:09 +0800

    get rid of default value in derive macro

--- crates/config_derive/src/lib.rs
+++ crates/config_derive/src/lib.rs
@@ -41,29 +41,20 @@ pub fn derive_config(input: TokenStream) -> TokenStream {
 
 	let mut field_ast = quote!();
 
-
-    // let aa: Attribute = parse_quote! {#[derive(ConfigDerive)]};
-    // eprintln!("{:#?}", &id);
-    // eprintln!("{:#?}", &s);
     for field in s.fields.iter(){
         let (field_id, field_attrs, field_ty) = (field.ident.as_ref().unwrap(), &field.attrs, &field.ty);
-        // let is_value = field_attrs.contains(&parse_quote! {#[derive(ConfigDerive)]});
         let is_Value = is_Value(field_ty);
-        // assert_eq!(field_id.type_id(),TypeId::of::<String>());
-        // eprintln!("{:#?}", is_Value);
 
         let field_name_literal = field_id.to_string();
         if !is_Value {
             field_ast.extend( quote! {
                 #field_id: CONFIGL.get(#field_name_literal)
                                     .map_or(#field_ty::new(),|v|v.clone().unwrap().into()),
-                // #field_id: #field_ty::default(),
             })
         }else {
             field_ast.extend( quote! {
                 #field_id: CONFIGL.get(#field_name_literal)
                                     .unwrap().clone().unwrap(),
-                // #field_id: #field_ty::default(),
             })
         }
     }
